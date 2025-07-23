a52c4767d5d62d7b77bb4c3d8b2dc54570baf98b
Harshini Domala <harshinidomala@harshinis-air-2.mynetworksettings.com>
2025-07-22 22:52:57 -0400

    greeting script

---
+++ qwerty.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to your GitHub project."
+
+if __name__ == "__main__":
+    user = input("Enter your name: ")
+    print(greet(user))
+    
+    
+    
+    
